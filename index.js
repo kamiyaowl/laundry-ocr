9742b92beaeb754228c7ef65e45e47e4ff3b21a6
K <psoc5lp@gmail.com>
2018-10-24 00:42:27 +0900

    config導入

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
+const config = require("config");
 const NodeWebcam = require( "node-webcam" );
-let opts = {
-    width: 1280,
-    height: 720,
-    quality: 100,
-    delay: 0,
-    saveShots: true,
-    output: "jpeg",
-    device: '/dev/video1',
-    callbackReturn: "location",
-    verbose: false
-};
+let opts = config.cameraOption;
 
 // const Webcam = NodeWebcam.create( opts );
 NodeWebcam.capture( "capture", opts, function( err, filename ) {
