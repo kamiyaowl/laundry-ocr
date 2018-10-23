812bbcf35a659022e425ee45c21cd8162d4bcd4f
K <psoc5lp@gmail.com>
2018-10-24 01:00:41 +0900

    onCaptureに分離

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const config = require("config");
+const cron = require('cron').CronJob;
 const NodeWebcam = require( "node-webcam" );
-let opts = config.cameraOption;
 
-// const Webcam = NodeWebcam.create( opts );
-NodeWebcam.capture( "capture", opts, function( err, filename ) {
-    console.log(err, filename);
-});
+// 写真撮影して保存する
+const onCapture = async (opts) => {
+    try {
+        const result = await NodeWebcam.capture( "capture", opts);
+        return "capture.jpg"; // TODO:もっとやる気が必要
+    } catch(e) {
+        console.error(e);
+        return null;
+    }
+};
+
+
+// load config and envs
+const caperaOption = config.cameraOption;
+const webhookUrls = process.env.WEBHOOK_URLS | config.webhookUrls | ""; // スペース区切りでURL複数指定可能
+const cronTime = process.env.CRON_TIME | config.cronTime; // 指定されなかった場合ワンショット実行する
+
+if (cronTime) {
+    // 定期実行
+} else {
+    (async () => {
+        console.log('onshot run');
+        const filename = await onCapture(caperaOption);
+        console.log(filename);
+        console.log('done');
+    })();
+}
