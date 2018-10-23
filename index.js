cba015998d29b16e3df1e294939d9096e2c92f0f
K <psoc5lp@gmail.com>
2018-10-24 01:08:00 +0900

    webhookUrlsを指定できるように修正

--- index.js
+++ index.js
@@ -9,15 +9,28 @@ const onCapture = async (opts) => {
         return "capture.jpg"; // TODO:もっとやる気が必要
     } catch(e) {
         console.error(e);
-        return null;
+        return false;
+    }
+};
+// Webhook先に通知できるように叩く
+const onPost = async (imagepath, urls) => {
+    if(!imagepath) {
+        console.error("no imagepath");
+        return false;
+    }
+    if(!urls || !urls.length) {
+        console.warn('urls are empty');
+        return false;
     }
 };
 
-
 // load config and envs
 const caperaOption = config.cameraOption;
-const webhookUrls = process.env.WEBHOOK_URLS | config.webhookUrls | ""; // スペース区切りでURL複数指定可能
-const cronTime = process.env.CRON_TIME | config.cronTime; // 指定されなかった場合ワンショット実行する
+const webhookUrls = // スペース区切りでURL複数指定可能
+    (process.env.WEBHOOK_URLS || config.webhookUrls || "")
+        .split(" ")
+        .filter(x => x); 
+const cronTime = process.env.CRON_TIME || config.cronTime; // 指定されなかった場合ワンショット実行する
 
 if (cronTime) {
     // 定期実行
@@ -25,7 +38,7 @@ if (cronTime) {
     (async () => {
         console.log('onshot run');
         const filename = await onCapture(caperaOption);
-        console.log(filename);
+        const result = await onPost(filename, webhookUrls);
         console.log('done');
     })();
 }
