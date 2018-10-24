05124a67cb04740aab21f6a4df86f8c50eb69fe8
K <kamiyaowl@gmail.com>
2018-10-24 12:33:27 +0900

    formatted

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const config = require("config");
-const cron = require('cron').CronJob;
-const NodeWebcam = require( "node-webcam" );
+const cron = require("cron").CronJob;
+const NodeWebcam = require("node-webcam");
 
 // 写真撮影して保存する
 const onCapture = async (opts) => {
     try {
-        const result = await NodeWebcam.capture( "capture", opts);
+        const result = await NodeWebcam.capture("capture", opts);
         return "capture.jpg"; // TODO:もっとやる気が必要
-    } catch(e) {
+    } catch (e) {
         console.error(e);
         return false;
     }
 };
 // Webhook先に通知できるように叩く
 const onPost = async (imagepath, urls) => {
-    if(!imagepath) {
+    if (!imagepath) {
         console.error("no imagepath");
         return false;
     }
-    if(!urls || !urls.length) {
+    if (!urls || !urls.length) {
         console.warn('urls are empty');
         return false;
     }
@@ -29,7 +29,7 @@ const caperaOption = config.cameraOption;
 const webhookUrls = // スペース区切りでURL複数指定可能
     (process.env.WEBHOOK_URLS || config.webhookUrls || "")
         .split(" ")
-        .filter(x => x); 
+        .filter(x => x);
 const cronTime = process.env.CRON_TIME || config.cronTime; // 指定されなかった場合ワンショット実行する
 
 if (cronTime) {
